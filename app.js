3ab4c2057e9650ef61847d27ed7c3cf15fdd9c2f
Pia <pia0413@gmail.com>
2022-10-23 23:33:03 +0900

    meme-maker final created

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const saveBtn = document.getElementById("save");
-const textInput = document.getElementById("text");
-const fileInput = document.getElementById("file");
-const modeBtn = document.getElementById("mode-btn");
-const destroyBtn = document.getElementById("destroy-btn");
+// mode
+const penBtn = document.getElementById("pen-btn");
+const shapeBtn = document.getElementById("shape-btn");
+const rectangleBtn = document.getElementById("rectangle-btn");
+const circleBtn = document.getElementById("circle-btn");
+
 const eraserBtn = document.getElementById("eraser-btn");
+const fillBtn = document.getElementById("fill-btn");
+const fileInput = document.getElementById("file");
+const resetBtn = document.getElementById("reset-btn");
+
+// text
+const textInput = document.getElementById("text");
+const fontTypes = document.getElementById("fontTypes");
+const fontSizes = document.getElementById("fontSizes");
+const fontEffects = document.getElementById("fontEffects");
+
+// save
+const saveBtn = document.getElementById("save");
+
+//color
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
 const color = document.getElementById("color");
+
+//brush
 const lineWidth = document.getElementById("line-width");
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -22,24 +40,48 @@ ctx.lineCap = "round";
 
 let isPainting = false;
 let isFilling = false;
+let mode = 0;
+
+let startX, startY;
 
 function onMove(event) {
   if (isPainting) {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-    return;
+    if (mode === 2) {
+      ctx.fillRect(
+        startX,
+        startY,
+        event.offsetX - startX,
+        event.offsetY - startY
+      );
+      return;
+    } else if (mode === 3) {
+      ctx.arc(startX, startY, event.offsetX - startX, 0, 2 * Math.PI);
+      ctx.fill();
+      return;
+    } else if (mode === 0 || mode === 2 || mode === 5) {
+      ctx.lineTo(event.offsetX, event.offsetY);
+      ctx.stroke();
+      return;
+    }
   }
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function startPainting() {
+function startPainting(event) {
   isPainting = true;
+  startX = event.offsetX;
+  startY = event.offsetY;
 }
 
-function cancelPainting() {
-  isPainting = false;
-  ctx.fill();
-  ctx.beginPath();
+function cancelPainting(event) {
+  if (mode !== 5) {
+    isPainting = false;
+    ctx.beginPath();
+  } else {
+    isPainting = false;
+    ctx.fill();
+    ctx.beginPath();
+  }
 }
 
 function onLineWidthChange(event) {
@@ -58,34 +100,43 @@ function onColorClick(event) {
   color.value = colorValue;
 }
 
-function onModeClick() {
-  if (isFilling) {
-    isFilling = false;
-    modeBtn.innerText = "Fill";
-  } else {
-    isFilling = true;
-    modeBtn.innerText = "Draw";
-  }
-}
-
 function onCanvasClick() {
   if (isFilling) {
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    isFilling = false;
+  } else if (mode === 4) {
+    const text = textInput.value;
+    const textFont = fontTypes.value;
+    const textSize = fontSizes.value;
+    const textEffect = fontEffects.value;
+    if (text !== "" && textEffect === "fill") {
+      ctx.save();
+      ctx.lineWidth = 1;
+      ctx.font = `${textSize}px ${textFont}`;
+      ctx.fillText(text, event.offsetX, event.offsetY);
+      ctx.restore();
+    } else if (textEffect === "stroke") {
+      ctx.save();
+      ctx.lineWidth = 1;
+      ctx.font = `${textSize}px ${textFont}`;
+      ctx.strokeText(text, event.offsetX, event.offsetY);
+      ctx.restore();
+    }
   }
 }
 
-function onDestroyClick() {
+function onClickReset() {
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
-function onEraseClick() {
+function onClickErase() {
   ctx.strokeStyle = "white";
   isFilling = false;
-  modeBtn.innerText = "Fill";
+  mode = 0;
 }
 
-function onFileChange(event) {
+function onClickFile(event) {
   const file = event.target.files[0];
   const url = URL.createObjectURL(file);
   const image = new Image();
@@ -96,18 +147,7 @@ function onFileChange(event) {
   };
 }
 
-function onDoubleClick(event) {
-  const text = textInput.value;
-  if (text !== "") {
-    ctx.save();
-    ctx.lineWidth = 1;
-    ctx.font = "68px sans-serif";
-    ctx.fillText(text, event.offsetX, event.offsetY);
-    ctx.restore();
-  }
-}
-
-function onSaveClick() {
+function onClickSave() {
   const url = canvas.toDataURL();
   const a = document.createElement("a");
   a.href = url;
@@ -115,7 +155,34 @@ function onSaveClick() {
   a.click();
 }
 
-canvas.addEventListener("dblclick", onDoubleClick);
+function onClickPen() {
+  mode = 0;
+}
+
+function onClickFill() {
+  mode = 1;
+  isFilling = true;
+}
+
+function onClickRectangle() {
+  mode = 2;
+  canvas.style.cursor = "crosshair";
+}
+
+function onClickCircle() {
+  mode = 3;
+  canvas.style.cursor = "crosshair";
+}
+
+function onClickText() {
+  mode = 4;
+  canvas.style.cursor = "text";
+}
+
+function onClickShape() {
+  mode = 5;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -126,8 +193,16 @@ color.addEventListener("change", onColorChange);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 
-modeBtn.addEventListener("click", onModeClick);
-destroyBtn.addEventListener("click", onDestroyClick);
-eraserBtn.addEventListener("click", onEraseClick);
-fileInput.addEventListener("change", onFileChange);
-saveBtn.addEventListener("click", onSaveClick);
+penBtn.addEventListener("click", onClickPen);
+shapeBtn.addEventListener("click", onClickShape);
+rectangleBtn.addEventListener("click", onClickRectangle);
+circleBtn.addEventListener("click", onClickCircle);
+
+eraserBtn.addEventListener("click", onClickErase);
+fillBtn.addEventListener("click", onClickFill);
+fileInput.addEventListener("change", onClickFile);
+resetBtn.addEventListener("click", onClickReset);
+
+textInput.addEventListener("click", onClickText);
+
+saveBtn.addEventListener("click", onClickSave);
